27786ac2c1505bb967fc0ad94140ff09d05f2096
Cliff Ingham <inghamn@bloomington.in.gov>
2019-10-22 13:50:49 -0400

    Web application now checks for user in HMAC request
    Updates #12

--- backend/src/Web/Users/Controllers/MyAccountController.php
+++ backend/src/Web/Users/Controllers/MyAccountController.php
@@ -7,6 +7,7 @@ declare (strict_types=1);
 
 namespace Web\Users\Controllers;
 
+use Web\Authentication\Auth;
 use Web\Controller;
 use Web\View;
 use Web\Users\Views\InfoView;
@@ -15,7 +16,11 @@ class MyAccountController extends Controller
 {
     public function __invoke(array $params): View
     {
-        $user = $this->di->get('currentUser');
-        return new InfoView($user);
+        $user = Auth::getAuthenticatedUser($this->di->get('Web\Authentication\AuthenticationService'));
+        if ($user) {
+            return new InfoView($user);
+        }
+
+        return new \Web\Views\ForbiddenView();
     }
 }
